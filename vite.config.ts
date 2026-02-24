06c500ff69ece42ccc5dcc039f297d32e2b86e56
ston39 <tonkimsanh@gmail.com>
2026-02-24 09:57:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/Asset-Tracker-ST/',
-  // ...keep your existing plugins etc.
+
+  plugins: [react()],
 })
