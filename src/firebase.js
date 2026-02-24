4ad0a19793859b1ab5ced14b16ee2399d5617dbc
ston39 <tonkimsanh@gmail.com>
2026-02-24 11:21:40 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDIyGG-adibv3j7enTocrvORf07f8MU2qw",
+  authDomain: "asset-tracker-st.firebaseapp.com",
+  projectId: "asset-tracker-st",
+  storageBucket: "asset-tracker-st.firebasestorage.app",
+  messagingSenderId: "289793239958",
+  appId: "1:289793239958:web:b72b1d7b385ee755da0bb7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
